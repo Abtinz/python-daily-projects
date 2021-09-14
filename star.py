a6c45e94eee81856ddfeca42a0c40e8ccd2a37ff
Abtinz <abtinzandi@gmail.com>
2021-09-14 15:35:20 +0430

    STAR *

---
+++ star.py
@@ -0,0 +1,31 @@
+def men(rowNumber) :
+    print("-----------------------------------")
+    print("men section")
+    for i in range(1,rowNumber+1) : print("*"*i)
+    print("-----------------------------------")
+    for i in range(1,rowNumber+1) : print("$"*i)
+    print("-----------------------------------")
+    for i in range(1,rowNumber+1) : print("#"*i)
+
+def legends():
+    print("-----------------------------------")
+    print("legends section")
+    print("*")  
+    print("**")  
+    print("***")
+    print("****")
+    print("*****")
+    print("******")
+    print("*******")
+    print("********")
+    print("*********")
+    print("**********")
+    print("***********")
+
+def main():
+    while 1 == 1 :
+        print("1)main star")
+        print("2) legend star")
+        print("3) EXIT")
+        choice=input()
+    
