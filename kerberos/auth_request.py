b7c959a72524a58abaf1056a7bebec29cc26bf04
Abtinz <abtinzandi@gmail.com>
2024-10-20 21:19:16 +0330

    code cleaning

--- kerberos/auth_request.py
+++ kerberos/auth_request.py
@@ -3,11 +3,11 @@
 
 import requests
 from requests_kerberos import HTTPKerberosAuth
+
+##HTTPKerberosAuth
 r = requests.get("https://abtinzandi81.org", auth=HTTPKerberosAuth())
 
 ##Password Authentication
-import requests
-from requests_kerberos import HTTPKerberosAuth, REQUIRED
 kerberos_auth = HTTPKerberosAuth(
         principal="abtin@REALM",
         password="14785487963/*/*/ccc",
@@ -16,21 +16,4 @@ r = requests.get("http://abtinzandi81.org", auth=kerberos_auth)
 
 
 ##Delegation
-import requests
-from requests_kerberos import HTTPKerberosAuth
 r = requests.get("http://abtinzandi81.org", auth=HTTPKerberosAuth(delegate=True))
-
-
-__, krb_context = kerberos.authGSSClientInit("HTTP@krbhost.example.com")
-
-kerberos.authGSSClientStep(krb_context, "")
-
-
-negotiate_details = kerberos.authGSSClientResponse(krb_context)
-
-headers = {"Authorization": "Negotiate " + negotiate_details}
-
-r = requests.get("https://krbhost.example.com/krb/", headers=headers)
-
-print(r.status_code , r.json)
-
